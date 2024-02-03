42bc518e40fdf6f737c9467651cfce8cb42bf0a3
DrewEandi27 <drew.eandi@gmail.com>
2024-02-03 09:47:01 -0800

    finalized gpio_interface

--- gpio_interface/gpio_interface.py
+++ gpio_interface/gpio_interface.py
@@ -8,61 +8,20 @@ class Gpio_interface(Node):
         super().__init__('gpio_interface')
 
         RPi.GPIO.setmode(RPi.GPIO.BCM)
-        
+
         self.gpio_pins = [16,17,18,19,22,25]
 
         for i in self.gpio_pins:
             RPi.GPIO.setup(i, RPi.GPIO.OUT)
-            self.declare(str(i), False)
+            self.declare_parameter(str(i), False)
 
-        #RPi.GPIO.setup(16, RPi.GPIO.OUT)
-        #RPi.GPIO.setup(17, RPi.GPIO.OUT)
-        #RPi.GPIO.setup(18, RPi.GPIO.OUT)
-        #RPi.GPIO.setup(19, RPi.GPIO.OUT)
-        #RPi.GPIO.setup(22, RPi.GPIO.OUT)
-        #RPi.GPIO.setup(25, RPi.GPIO.OUT)
-
-        self.log=self.get_logger()
-
-        #self.declare_parameter('17', False)
-        #self.declare_parameter('18', False)
-        #self.declare_parameter('19', False)
-        #self.declare_parameter('22', False)
-        #self.declare_parameter('25', False)
-        
+        self.log=self.get_logger()        
         self.add_on_set_parameters_callback(self.function)
 
     def function(self, params):
         for param in params:
-            if param.name == '16':
-
-                RPi.GPIO.output(16, param.value)
-                self.log.info(str(param.value))
-
-            if param.name == '17':
-
-                RPi.GPIO.output(17, param.value)
-                self.log.info(str(param.value))
-
-            if param.name == '18':
-
-                RPi.GPIO.output(18, param.value)
-                self.log.info(str(param.value))
-
-            if param.name == '19':
-
-                RPi.GPIO.output(19, param.value)
-                self.log.info(str(param.value))
-
-            if param.name == '22':
-
-                RPi.GPIO.output(22, param.value)
-                self.log.info(str(param.value))
-
-            if param.name == '25':
-
-                RPi.GPIO.output(25, param.value)
-                self.log.info(str(param.value))
+            RPi.GPIO.output(int(param.name), param.value)
+            self.log.info(str(param.value))
 
         return SetParametersResult(successful=True)
 
